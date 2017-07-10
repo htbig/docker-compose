1124e0f66007a3c5ee688d662561b2dabbcd7743
cc <cc@qq.com>
2017-07-10 11:24:42 +0800

    base

---
+++ root/.bash_history
@@ -0,0 +1,15 @@
+cd var/
+cd lib/
+ls
+pwd
+ls
+cd kapacitor/
+ls
+ls -l
+car cluster.id 
+cat cluster.id 
+cd replay/
+ls
+cd ..
+ls
+exit
---
+++ root/cpu_alert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl --request POST \
+  --url http://m.vegcloud.tech:669/wechatNotify/message/ \
+  --header 'authorization: Basic YWRtaW46Y2poeXkzMDA=' \
+  --header 'cache-control: no-cache' \
+  --header 'content-type: application/json' \
+  --header 'postman-token: b094cf6e-9aec-fe39-5339-c791feefe910' \
+  --data '{"messages": "10 device, cpu_idle is less than 100","user": "HeTing"}'
+
